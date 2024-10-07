0fb42bb8b8ffa3d0f0a7358885dd0a5f3c658105
that-guy-ralf <r.a.laflamme@protonmail.com>
2024-10-07 13:17:16 -0400

    changes to alpha, removal of some plugins

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,31 +1,31 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim"
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim'
     },
     config = function()
-      vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>')
-      vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-      require("neo-tree").setup({
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle right<CR>', {})
+      vim.keymap.set('n', '<leader>bf', ':Neotree buffers reveal float<CR>', {})
+      require('neo-tree').setup({
         sources = {
-          "filesystem",
-          "buffers",
-          "git_status",
+          'filesystem',
+          'buffers',
+          'git_status',
         },
         source_selector = {
           winbar = true,
           statusline = false, -- toggle to show selector on statusline
-          content_layout = "center",
-          tabs_layout = "equal",
+          content_layout = 'center',
+          tabs_layout = 'equal',
           sources = {
-            { source = "filesystem", display_name = "Files" },
-            { source = "buffers", display_name = "Buffers" },
-            { source = "git_status", display_name = "Git" },
+            { source = 'filesystem', display_name = 'Files' },
+            { source = 'buffers', display_name = 'Buffers' },
+            { source = 'git_status', display_name = 'Git' },
           },
         },
     })
-    end
+    end,
 }
